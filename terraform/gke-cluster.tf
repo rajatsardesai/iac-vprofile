eb5cfd4c4034a3c6d14343ce790eb043c7747e09
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-15 22:27:46 +0530

    Update subnetwork value

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -5,7 +5,7 @@ module "gke" {
   name               = local.cluster_name
   region             = var.region
   network            = module.vpc.network_name
-  subnetwork         = module.vpc.subnets[0].public-subnet-1
+  subnetwork         = module.vpc.subnets["public-subnet-1"].subnet_name
   ip_range_pods      = "us-central1-01-gke-01-pods"
   ip_range_services  = "us-central1-01-gke-01-services"
   initial_node_count = 2
