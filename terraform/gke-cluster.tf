335a1a0d38a6d7e9b1df446ce158027d1d87add6
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-15 22:04:14 +0530

    Added ip_range_pods and ip_range_services inputs

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -6,6 +6,8 @@ module "gke" {
   region             = var.region
   network            = module.vpc.network_name
   subnetwork         = module.vpc.private_subnets[0]
+  ip_range_pods      = "us-central1-01-gke-01-pods"
+  ip_range_services  = "us-central1-01-gke-01-services"
   initial_node_count = 2
 
   node_pools = [
