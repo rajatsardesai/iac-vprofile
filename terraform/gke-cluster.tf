9485f5321db301e3bf3185c2e5c1550c8bcce303
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-16 10:44:28 +0530

    Test workflow

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -2,10 +2,10 @@ module "gke" {
   source     = "terraform-google-modules/kubernetes-engine/google"
   project_id = var.project_id
 
-  name    = local.cluster_name
-  region  = var.region
-  network = module.vpc.network_name
-  subnetwork = module.vpc.module.subnets.google_compute_subnetwork.subnetwork["us-central1/private-subnet-1"]
+  name               = local.cluster_name
+  region             = var.region
+  network            = module.vpc.network_name
+  subnetwork         = module.vpc.module.subnets.google_compute_subnetwork.subnetwork["us-central1/private-subnet-1"]
   ip_range_pods      = "172.20.0.0/16"
   ip_range_services  = "172.20.7.0/26"
   initial_node_count = 2
