8c61f5248507204ed8c3104298b46a28f94ae894
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-16 17:19:27 +0530

    Iterated values from secondary ranges and assigned it to the ip_range_pods and ip_range_services

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -2,12 +2,16 @@ module "gke" {
   source     = "terraform-google-modules/kubernetes-engine/google"
   project_id = var.project_id
 
-  name               = local.cluster_name
-  region             = var.region
-  network            = module.vpc.network_name
-  subnetwork         = "public-subnet-1"
-  ip_range_pods      = module.vpc.secondary_ranges.subnet-pods.range_name
-  ip_range_services  = module.vpc.secondary_ranges.subnet-services.range_name
+  name       = local.cluster_name
+  region     = var.region
+  network    = module.vpc.network_name
+  subnetwork = "public-subnet-1"
+  ip_range_pods = {
+    for range in secondary_ranges.subnet-pods : range.range_name => range.ip_cidr_range
+  }
+  ip_range_services = {
+    for range in secondary_ranges.subnet-services : range.range_name => range.ip_cidr_range
+  }
   initial_node_count = 2
 
   node_pools = [
