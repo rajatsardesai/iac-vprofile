e04a451b1605ac3f54f8b3a09285876e3de78953
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-16 19:27:56 +0530

    Changed location from region to zone

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP region"
   type        = string
-  default     = "us-central1"
+  default     = "us-central1-b"
 }
 
 variable "clusterName" {
