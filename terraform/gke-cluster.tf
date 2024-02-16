dd324299a2fc02d77ebaf62ef0f5e09e04935c03
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-16 12:10:02 +0530

    Test workflow

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -5,7 +5,7 @@ module "gke" {
   name               = local.cluster_name
   region             = var.region
   network            = module.vpc.network_name
-  subnetwork         = module.vpc.subnets[countindex("public-subnet-1")]
+  subnetwork         = module.vpc.subnets
   ip_range_pods      = "172.20.0.0/16"
   ip_range_services  = "172.20.7.0/26"
   initial_node_count = 2
