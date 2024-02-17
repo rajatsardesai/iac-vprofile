40bf23c798406e9f4297a5f2f80c806e56d546a6
Rajat Sardesai <rajatsardesai@gmail.com>
2024-02-17 22:37:18 +0530

    Change in ip

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,42 +8,42 @@ module "vpc" {
   subnets = [
     {
       subnet_name           = "public-subnet-1"
-      subnet_ip             = "172.16.2.0/24"
+      subnet_ip             = "172.16.1.0/24"
       subnet_region         = var.region
       subnet_private_access = false
       subnet_flow_logs      = false
     },
     {
       subnet_name           = "public-subnet-2"
-      subnet_ip             = "172.16.3.0/24"
+      subnet_ip             = "172.16.2.0/24"
       subnet_region         = var.region
       subnet_private_access = false
       subnet_flow_logs      = false
     },
     {
       subnet_name           = "public-subnet-3"
-      subnet_ip             = "172.16.4.0/24"
+      subnet_ip             = "172.16.3.0/24"
       subnet_region         = var.region
       subnet_private_access = false
       subnet_flow_logs      = false
     },
     {
       subnet_name           = "private-subnet-1"
-      subnet_ip             = "172.16.5.0/24"
+      subnet_ip             = "172.16.4.0/24"
       subnet_region         = var.region
       subnet_private_access = true
       subnet_flow_logs      = false
     },
     {
       subnet_name           = "private-subnet-2"
-      subnet_ip             = "172.16.6.0/24"
+      subnet_ip             = "172.16.5.0/24"
       subnet_region         = var.region
       subnet_private_access = true
       subnet_flow_logs      = false
     },
     {
       subnet_name           = "private-subnet-3"
-      subnet_ip             = "172.16.7.0/24"
+      subnet_ip             = "172.16.6.0/24"
       subnet_region         = var.region
       subnet_private_access = true
       subnet_flow_logs      = false
@@ -54,11 +54,11 @@ module "vpc" {
     public-subnet-2 = [
       {
         range_name    = "pods-range"
-        ip_cidr_range = "172.16.0.0/24"
+        ip_cidr_range = "192.168.0.0/20"
       },
       {
         range_name    = "services-range"
-        ip_cidr_range = "172.20.1.0/24"
+        ip_cidr_range = "10.0.0.0/20"
       }
     ]
   }
